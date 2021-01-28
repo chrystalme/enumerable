c0ee76827a56ca3e5b5103faf0d72a887c7688ed
chrystalme <afam.ifeanyi@gmail.com>
2021-01-28 14:08:03 +0200

    Add rspec

---
+++ spec/enumerable_spec.rb
@@ -0,0 +1,11 @@
+require_relative '../enumerable'
+
+describe Enumerable do
+  let(:num_array) {[1,2,3,4,5,6,7,8]}
+  let(:range) {(1..8)}
+  let(:friends) {%w[Sharon Leo Leila Brian Arun]}
+
+  describe '#my_each' do
+    
+  end
+end
